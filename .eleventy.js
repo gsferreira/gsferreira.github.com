624d47375079d849c735bbfa6b9160a5ebbff41b
Guilherme Ferreira <guilherme.as.ferreira@gmail.com>
2020-08-10 22:01:25 +0100

    Convert report uri wizard into report-only

--- .eleventy.js
+++ .eleventy.js
@@ -49,10 +49,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/CNAME");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/netlify.toml");
-  eleventyConfig.addPassthroughCopy(
-    "node_modules/dracula-prism/css/dracula-prism.css",
-    "src/_css/dracula-prism.css"
-  );
 
   return {
     dir: {
