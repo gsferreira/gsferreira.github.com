eac14a5975f391c5f4192afe699798467635d1a2
Gui Ferreira <guilherme.as.ferreira@gmail.com>
2023-11-07 18:01:53 +0000

    Add courses page (#14)

---
+++ src/_data/courses.js
@@ -0,0 +1,10 @@
+module.exports = {
+  all: [
+    {
+      title: "From Zero to Hero: Test-Driven Development in C#",
+      description:
+        "Learn the transformational practice that will lead you to simple code, high quality and peace of mind.",
+      url: "https://dometrain.com/course/from-zero-to-hero-test-driven-development-tdd-csharp?affcode=1115529_k5a22dj8",
+    },
+  ],
+};
