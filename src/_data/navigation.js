78e5d2c1248dc5c5502f1e2ef986f5518314df62
Guilherme Ferreira <guilherme.as.ferreira@gmail.com>
2023-11-07 08:21:12 +0000

    Add courses page

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -2,6 +2,7 @@ module.exports = {
   main: [
     { label: "Home", url: "/", external: false },
     { label: "Blog", url: "/blog/", external: false },
+    { label: "Courses", url: "/courses/", external: false },
     { label: "About", url: "/about/", external: false },
   ],
 };
