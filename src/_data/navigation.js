f1d80ea8b15d19ac30ec8abd4404765f9726eeb1
Guilherme Ferreira <guilherme.as.ferreira@gmail.com>
2023-11-22 22:08:30 +0000

    fix navigation

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,9 +1,9 @@
 module.exports = {
   main: [
     { label: "Home", url: "/", external: false },
+    { label: "About", url: "/about/", external: false },
     { label: "Blog", url: "/blog/", external: false },
     { label: "Courses", url: "/courses/", external: false },
     { label: "Talks", url: "/talks/", external: false },
-    { label: "About", url: "/about/", external: false },
   ],
 };
