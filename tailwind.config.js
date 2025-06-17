13b40025140d3c53b221e46517d4718ff3d60bcb
Guilherme Ferreira <guilherme.as.ferreira@gmail.com>
2025-06-17 21:15:19 +0100

    darkmode

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.liquid',
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}; 
