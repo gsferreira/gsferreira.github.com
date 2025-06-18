6028f6773ecd3e0567f89560f3781d4cfe2011d5
Guilherme Ferreira <guilherme.as.ferreira@gmail.com>
2025-06-18 22:26:30 +0100

    about page

---
+++ src/_data/talks.js
@@ -0,0 +1,137 @@
+module.exports = {
+    all: [
+      {
+        title: "How to fall in love with TDD",
+        description: `Have you tried Test-Driven Development (TDD)? Do you think it's not for you? Was it leading to over-testing and unnecessary complexity? Tests became a maintenance burden? Does it slow down the development process? Due to industry misconceptions, all that is normal. 
+  
+        I have good news. We can still change that. You can still reach enlightenment and become a TDD lover.
+        
+        This talk tells you the story of what will happen once you decide to learn TDD. It will go through the challenges you will face in your journey and how to avoid being trapped by them. We'll debunk misconceptions and see an effective method of practising TDD.
+        
+        If you are open to becoming TDD addicted, this session is for you. You can expect to go home inspired to master the testing craft.
+        
+        Be prepared to be Test Infected. Beware, there's no cure!`,
+        presentations: [
+          {
+            name: "NDC Porto 2024",
+            url: "https://ndcporto.com/",
+            date: "2024-10-16"
+          },
+          {
+            name: "NDC Oslo 2024",
+            url: "https://ndcoslo.com/",
+            date: "2024-06-13",
+            recording: "https://youtu.be/vXnTMjMe5Eg?si=NdPpNuxRqYEerFdn",
+          },
+          {
+            name: "NDC London 2024",
+            url: "https://ndclondon.com/",
+            date: "2024-02-01",
+            recording: "https://youtu.be/UkhZkK6f5Xw?si=mQ_BkVtzeyDFVa8U",
+          },
+        ],
+      },
+      {
+        title: "Apache Kafka in 1 hour for C# Developers",
+        description: `Are you a Developer curious about Apache Kafka? Have you never had the time to dig into it?
+  
+  No problem. In this session, we will dig into it. We will go on a journey to unveil how I've built an event-driven system to power my Patreon account using Kafka. We will focus on the parts you need to know to build Event-Driven Applications. Explore the basic concepts and recommendations. And learn about how to use C# and KafkaFlow, an Open Source framework for .NET. 
+  
+  Even if you know nothing about it, you can expect to go home with the basics to start your Kafka journey.`,
+        presentations: [
+          {
+            name: "AZUGPT Meetup",
+            url: "https://www.meetup.com/azure-user-group-portugal/events/289695622/",
+            date: "2023-01-18",
+          },
+          {
+            name: "NDC London 2023",
+            url: "https://ndclondon.com/",
+            date: "2023-01-26",
+            recording: "https://youtu.be/4xpjlqIlfY8?si=F5_fcRE-xtngYv7K",
+          },
+          {
+            name: "NetPonto Porto Meetup",
+            url: "https://www.meetup.com/netponto-porto/events/291509299/",
+            date: "2023-02-18",
+          },
+          {
+            name: "Copenhagen Developers Festival 2023",
+            url: "https://cphdevfest.com/",
+            date: "2023-08-30",
+            recording: "https://www.youtube.com/watch?v=E07CGvGVal8",
+          },
+          {
+            name: "NDC Porto 2023",
+            url: "https://ndcporto.com/",
+            date: "2023-10-18",
+            recording: "https://youtu.be/H_dDTMz_x4E?si=pTP61Up1e4jrjxO3",
+          },
+        ],
+      },
+      {
+        title: "The Grand Unified Theory of Clean Architecture and Test Pyramid",
+        description: `Clean Architecture and the Test Pyramid are two important concepts in software development. But how can they be effectively combined?
+  
+  If you’ve explored them in depth, you know there are countless interpretations, so there’s no “one way to do it”. It seems impossible to find an effective way to know what types of tests to write.
+  As a result, knowing what types of tests and code to write is often overwhelming.
+  
+  In this highly opinionated talk, based on my experience, I’ll share a practical framework for combining them.
+  We’ll go back to Clean Architecture origins.
+  We will revisit testing practices, such as unit testing, solitary testing, or integration testing.
+  And by the end, we’ll have clear guidelines on when and where to apply different types of tests.
+  
+  If you have faced those challenges, this session is for you. You can expect to go home with a simple and actionable framework to tackle them.`,
+        presentations: [
+          {
+            name: "Copenhagen Developers Festival 2024",
+            url: "https://cphdevfest.com/",
+            date: "2024-08-28",
+            recording: "https://youtu.be/mzznsq4jCHY?si=CC1PM_2ykQYA9TbJ",
+          },
+          {
+            name: "NDC Porto 2022",
+            url: "https://ndcporto.com/",
+            date: "2022-04-28",
+            recording: "https://youtu.be/gHSpj2zM9Nw?si=2VAuRbwi_uZiCSGm",
+          },
+          {
+            name: "NDC Oslo 2022",
+            url: "https://ndcoslo.com/",
+            date: "2022-09-30",
+            recording: "https://youtu.be/8BuCzpfp8Pk?si=VZJTDwJH29zr-oxp",
+          },
+        ],
+      },
+      {
+        title: "Embracing Simplicity",
+        description: `Are you tired of juggling with complexity every single day? Don't you wish for a simple life?
+  
+        Technologists tend to have a passion for complexity. But because of that, we tend to overcomplicate when we face a new challenge.
+        
+        Finding simple solutions doesn't need to be a magic trick. In this Code free session, we will explore the value that Simplicity can bring to your life and how you can apply it in your day-to-day life.
+        
+        If you live in the software world, this session is for you. You can expect to go home with some handy Simplification tools.`,
+        presentations: [
+          {
+            name: "NDC Oslo 2020",
+            url: "https://ndcoslo.com/",
+            date: "2020-06-12",
+            recording: "https://youtu.be/LLdU3OnJ83Q?si=ib7azyGetzpXGYsF",
+          },
+          {
+            name: "NDC TechTown 2020",
+            url: "https://ndctechtown.com/",
+            date: "2020-08-31",
+            recording: "https://youtu.be/pLMKbNQdSoI?si=rdKxOyl4ChQIM2E5",
+          },
+          {
+            name: "NDC Porto 2022",
+            url: "https://ndcporto.com/",
+            date: "2022-04-29",
+            recording: "https://youtu.be/I76uhPaWY1o?si=gyfiT51ad-LBy0KY",
+          },
+        ],
+      },
+    ],
+  };
