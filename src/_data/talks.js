987e835b9998f6a7ea5f9bd7c939d750276b9790
Gui Ferreira <guilherme.as.ferreira@gmail.com>
2025-10-28 15:29:36 +0000

    Add new keynote 'Imagine If We Made It Simple' (#38)
    Introduced a new keynote talk 'Imagine If We Made It Simple' with presentation details for NDC Porto 2025. Refactored formatting for consistency and updated the talks list accordingly.

--- src/_data/talks.js
+++ src/_data/talks.js
@@ -1,8 +1,31 @@
 export default {
-    all: [
-      {
-        title: "How to fall in love with TDD",
-        description: `Have you tried Test-Driven Development (TDD)? Do you think it's not for you? Was it leading to over-testing and unnecessary complexity? Tests became a maintenance burden? Does it slow down the development process? Due to industry misconceptions, all that is normal. 
+  all: [
+    {
+      title: "Imagine If We Made It Simple",
+      description: `When was the last time you paused at work and thought: "How did this get so complicated?"
+
+We never planned to make our jobs harder. But somewhere between chasing new tools, planning for problems that may never come, jumping on every trend and trying to prepare for every future scenario, we lost our way. Today, building software isn't just complex. It's exhausting.
+The worst part? We did this to ourselves. Our habits, our pride, and our fear of missing out push us to add more and more, even when less would serve us better.
+
+But what if we chose another path? What if we stopped and asked: "How simple can this be?" Not simple as in easy or limited, but simple as in clear, focused, and built to last.
+
+This keynote isn't about magic fixes or secret frameworks. It's about seeing that simplicity is both a choice and an opportunity. It takes discipline, creativity, courage, and sometimes more work than the complex path.
+We'll explore why we tend to overcomplicate things and how to practice simplicity through Focus on what truly matters, Subtract what doesn't, and Master what remains.
+
+The next big shift in software won't come from AI or the next shiny tool.
+It will come from how we use them, and it starts with the next decision you make.`,
+      presentations: [
+        {
+          name: "NDC Porto 2025",
+          url: "https://ndcporto.com/",
+          date: new Date("2025-10-23"),
+          recording: "https://youtu.be/3brPDRvbIP8?si=Y3UADh-W91KAc8tN",
+        },
+      ],
+    },
+    {
+      title: "How to fall in love with TDD",
+      description: `Have you tried Test-Driven Development (TDD)? Do you think it's not for you? Was it leading to over-testing and unnecessary complexity? Tests became a maintenance burden? Does it slow down the development process? Due to industry misconceptions, all that is normal. 
   
 I have good news. We can still change that. You can still reach enlightenment and become a TDD lover.
         
@@ -11,67 +34,67 @@ This talk tells you the story of what will happen once you decide to learn TDD. 
 If you are open to becoming TDD addicted, this session is for you. You can expect to go home inspired to master the testing craft.
         
 Be prepared to be Test Infected. Beware, there's no cure!`,
-        presentations: [
-          {
-            name: "NDC Porto 2024",
-            url: "https://ndcporto.com/",
-            date: new Date("2024-10-16")
-          },
-          {
-            name: "NDC Oslo 2024",
-            url: "https://ndcoslo.com/",
-            date: new Date("2024-06-13"),
-            recording: "https://youtu.be/vXnTMjMe5Eg?si=NdPpNuxRqYEerFdn",
-          },
-          {
-            name: "NDC London 2024",
-            url: "https://ndclondon.com/",
-            date: new Date("2024-02-01"),
-            recording: "https://youtu.be/UkhZkK6f5Xw?si=mQ_BkVtzeyDFVa8U",
-          },
-        ],
-      },
-      {
-        title: "Apache Kafka in 1 hour for C# Developers",
-        description: `Are you a Developer curious about Apache Kafka? Have you never had the time to dig into it?
+      presentations: [
+        {
+          name: "NDC Porto 2024",
+          url: "https://ndcporto.com/",
+          date: new Date("2024-10-16")
+        },
+        {
+          name: "NDC Oslo 2024",
+          url: "https://ndcoslo.com/",
+          date: new Date("2024-06-13"),
+          recording: "https://youtu.be/vXnTMjMe5Eg?si=NdPpNuxRqYEerFdn",
+        },
+        {
+          name: "NDC London 2024",
+          url: "https://ndclondon.com/",
+          date: new Date("2024-02-01"),
+          recording: "https://youtu.be/UkhZkK6f5Xw?si=mQ_BkVtzeyDFVa8U",
+        },
+      ],
+    },
+    {
+      title: "Apache Kafka in 1 hour for C# Developers",
+      description: `Are you a Developer curious about Apache Kafka? Have you never had the time to dig into it?
   
 No problem. In this session, we will dig into it. We will go on a journey to unveil how I've built an event-driven system to power my Patreon account using Kafka. We will focus on the parts you need to know to build Event-Driven Applications. Explore the basic concepts and recommendations. And learn about how to use C# and KafkaFlow, an Open Source framework for .NET. 
   
 Even if you know nothing about it, you can expect to go home with the basics to start your Kafka journey.`,
-        presentations: [
-          {
-            name: "AZUGPT Meetup",
-            url: "https://www.meetup.com/azure-user-group-portugal/events/289695622/",
-            date: new Date("2023-01-18"),
-          },
-          {
-            name: "NDC London 2023",
-            url: "https://ndclondon.com/",
-            date: new Date("2023-01-26"),
-            recording: "https://youtu.be/4xpjlqIlfY8?si=F5_fcRE-xtngYv7K",
-          },
-          {
-            name: "NetPonto Porto Meetup",
-            url: "https://www.meetup.com/netponto-porto/events/291509299/",
-            date: new Date("2023-02-18"),
-          },
-          {
-            name: "Copenhagen Developers Festival 2023",
-            url: "https://cphdevfest.com/",
-            date: new Date("2023-08-30"),
-            recording: "https://www.youtube.com/watch?v=E07CGvGVal8",
-          },
-          {
-            name: "NDC Porto 2023",
-            url: "https://ndcporto.com/",
-            date: new Date("2023-10-18"),
-            recording: "https://youtu.be/H_dDTMz_x4E?si=pTP61Up1e4jrjxO3",
-          },
-        ],
-      },
-      {
-        title: "The Grand Unified Theory of Clean Architecture and Test Pyramid",
-        description: `Clean Architecture and the Test Pyramid are two important concepts in software development. But how can they be effectively combined?
+      presentations: [
+        {
+          name: "AZUGPT Meetup",
+          url: "https://www.meetup.com/azure-user-group-portugal/events/289695622/",
+          date: new Date("2023-01-18"),
+        },
+        {
+          name: "NDC London 2023",
+          url: "https://ndclondon.com/",
+          date: new Date("2023-01-26"),
+          recording: "https://youtu.be/4xpjlqIlfY8?si=F5_fcRE-xtngYv7K",
+        },
+        {
+          name: "NetPonto Porto Meetup",
+          url: "https://www.meetup.com/netponto-porto/events/291509299/",
+          date: new Date("2023-02-18"),
+        },
+        {
+          name: "Copenhagen Developers Festival 2023",
+          url: "https://cphdevfest.com/",
+          date: new Date("2023-08-30"),
+          recording: "https://www.youtube.com/watch?v=E07CGvGVal8",
+        },
+        {
+          name: "NDC Porto 2023",
+          url: "https://ndcporto.com/",
+          date: new Date("2023-10-18"),
+          recording: "https://youtu.be/H_dDTMz_x4E?si=pTP61Up1e4jrjxO3",
+        },
+      ],
+    },
+    {
+      title: "The Grand Unified Theory of Clean Architecture and Test Pyramid",
+      description: `Clean Architecture and the Test Pyramid are two important concepts in software development. But how can they be effectively combined?
   
 If you've explored them in depth, you know there are countless interpretations, so there's no "one way to do it". It seems impossible to find an effective way to know what types of tests to write.
 As a result, knowing what types of tests and code to write is often overwhelming.
@@ -82,56 +105,56 @@ We will revisit testing practices, such as unit testing, solitary testing, or in
 And by the end, we'll have clear guidelines on when and where to apply different types of tests.
   
 If you have faced those challenges, this session is for you. You can expect to go home with a simple and actionable framework to tackle them.`,
-        presentations: [
-          {
-            name: "Copenhagen Developers Festival 2024",
-            url: "https://cphdevfest.com/",
-            date: new Date("2024-08-28"),
-            recording: "https://youtu.be/mzznsq4jCHY?si=CC1PM_2ykQYA9TbJ",
-          },
-          {
-            name: "NDC Porto 2022",
-            url: "https://ndcporto.com/",
-            date: new Date("2022-04-28"),
-            recording: "https://youtu.be/gHSpj2zM9Nw?si=2VAuRbwi_uZiCSGm",
-          },
-          {
-            name: "NDC Oslo 2022",
-            url: "https://ndcoslo.com/",
-            date: new Date("2022-09-30"),
-            recording: "https://youtu.be/8BuCzpfp8Pk?si=VZJTDwJH29zr-oxp",
-          },
-        ],
-      },
-      {
-        title: "Embracing Simplicity",
-        description: `Are you tired of juggling with complexity every single day? Don't you wish for a simple life?
+      presentations: [
+        {
+          name: "Copenhagen Developers Festival 2024",
+          url: "https://cphdevfest.com/",
+          date: new Date("2024-08-28"),
+          recording: "https://youtu.be/mzznsq4jCHY?si=CC1PM_2ykQYA9TbJ",
+        },
+        {
+          name: "NDC Porto 2022",
+          url: "https://ndcporto.com/",
+          date: new Date("2022-04-28"),
+          recording: "https://youtu.be/gHSpj2zM9Nw?si=2VAuRbwi_uZiCSGm",
+        },
+        {
+          name: "NDC Oslo 2022",
+          url: "https://ndcoslo.com/",
+          date: new Date("2022-09-30"),
+          recording: "https://youtu.be/8BuCzpfp8Pk?si=VZJTDwJH29zr-oxp",
+        },
+      ],
+    },
+    {
+      title: "Embracing Simplicity",
+      description: `Are you tired of juggling with complexity every single day? Don't you wish for a simple life?
   
 Technologists tend to have a passion for complexity. But because of that, we tend to overcomplicate when we face a new challenge.
         
 Finding simple solutions doesn't need to be a magic trick. In this Code free session, we will explore the value that Simplicity can bring to your life and how you can apply it in your day-to-day life.
         
 If you live in the software world, this session is for you. You can expect to go home with some handy Simplification tools.`,
-        presentations: [
-          {
-            name: "NDC Oslo 2020",
-            url: "https://ndcoslo.com/",
-            date: new Date("2020-06-12"),
-            recording: "https://youtu.be/LLdU3OnJ83Q?si=ib7azyGetzpXGYsF",
-          },
-          {
-            name: "NDC TechTown 2020",
-            url: "https://ndctechtown.com/",
-            date: new Date("2020-08-31"),
-            recording: "https://youtu.be/pLMKbNQdSoI?si=rdKxOyl4ChQIM2E5",
-          },
-          {
-            name: "NDC Porto 2022",
-            url: "https://ndcporto.com/",
-            date: new Date("2022-04-29"),
-            recording: "https://youtu.be/I76uhPaWY1o?si=gyfiT51ad-LBy0KY",
-          },
-        ],
-      },
-    ],
-  };
+      presentations: [
+        {
+          name: "NDC Oslo 2020",
+          url: "https://ndcoslo.com/",
+          date: new Date("2020-06-12"),
+          recording: "https://youtu.be/LLdU3OnJ83Q?si=ib7azyGetzpXGYsF",
+        },
+        {
+          name: "NDC TechTown 2020",
+          url: "https://ndctechtown.com/",
+          date: new Date("2020-08-31"),
+          recording: "https://youtu.be/pLMKbNQdSoI?si=rdKxOyl4ChQIM2E5",
+        },
+        {
+          name: "NDC Porto 2022",
+          url: "https://ndcporto.com/",
+          date: new Date("2022-04-29"),
+          recording: "https://youtu.be/I76uhPaWY1o?si=gyfiT51ad-LBy0KY",
+        },
+      ],
+    },
+  ],
+};
